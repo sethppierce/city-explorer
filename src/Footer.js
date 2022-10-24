91b462e17e9239a9378519cd13d85bd6a81e3d17
Seth <sethparkerpierce@icloud.com>
2022-10-24 19:54:20 -0400

    completed lab06

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Footer.css'
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div>Author: Seth Pierce</div>
+      </footer>
+    )
+  }
+}
