04280122f5f8f10dc7dd6b994f3efe8eb35414e5
Seth <sethparkerpierce@icloud.com>
2022-10-27 16:07:05 -0400

    finished lab09

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -8,7 +8,7 @@ export default class WeatherData extends Component {
     return (
     <>
       <h1 id='weatherData'>WeatherData</h1>
-      <div id='weathercontainer'>
+      <div id='weathercontain'>
             {this.props.showWeatherErr ?
               this.props.weatherData.description.map((item, index) => {
                 return <WeatherAccord id='weatherDataItem' key={index} date={item.date} description={item.description}/>}): <p id='weatherErr'>{this.props.weatherErr.toUpperCase()}</p>
